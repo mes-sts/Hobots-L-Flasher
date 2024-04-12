2af3f6de1dd0c6995fdb1f2407cfb830feccea5d
Roman <romansklyar15@gmail.com>
2024-04-13 00:58:45 +0300

    добавил вкладки

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,52 +29,56 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
-            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
-            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.tabControllers = new MetroFramework.Controls.MetroTabControl();
+            this.tabControllerStandart = new MetroFramework.Controls.MetroTabPage();
+            this.tabControllerClassic = new MetroFramework.Controls.MetroTabPage();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
-            this.metroTabControl1.SuspendLayout();
+            this.tabControllerHyper = new MetroFramework.Controls.MetroTabPage();
+            this.tabControllerMini = new MetroFramework.Controls.MetroTabPage();
+            this.tabControllers.SuspendLayout();
             this.SuspendLayout();
             // 
-            // metroTabControl1
+            // tabControllers
             // 
-            this.metroTabControl1.Controls.Add(this.metroTabPage1);
-            this.metroTabControl1.Controls.Add(this.metroTabPage2);
-            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.metroTabControl1.Location = new System.Drawing.Point(20, 60);
-            this.metroTabControl1.Name = "metroTabControl1";
-            this.metroTabControl1.SelectedIndex = 0;
-            this.metroTabControl1.Size = new System.Drawing.Size(760, 366);
-            this.metroTabControl1.TabIndex = 0;
-            this.metroTabControl1.UseSelectable = true;
+            this.tabControllers.Controls.Add(this.tabControllerStandart);
+            this.tabControllers.Controls.Add(this.tabControllerClassic);
+            this.tabControllers.Controls.Add(this.tabControllerHyper);
+            this.tabControllers.Controls.Add(this.tabControllerMini);
+            this.tabControllers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControllers.Location = new System.Drawing.Point(20, 60);
+            this.tabControllers.Name = "tabControllers";
+            this.tabControllers.SelectedIndex = 0;
+            this.tabControllers.Size = new System.Drawing.Size(760, 366);
+            this.tabControllers.TabIndex = 0;
+            this.tabControllers.UseSelectable = true;
             // 
-            // metroTabPage1
+            // tabControllerStandart
             // 
-            this.metroTabPage1.HorizontalScrollbarBarColor = true;
-            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
-            this.metroTabPage1.HorizontalScrollbarSize = 10;
-            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
-            this.metroTabPage1.Name = "metroTabPage1";
-            this.metroTabPage1.Size = new System.Drawing.Size(752, 328);
-            this.metroTabPage1.TabIndex = 0;
-            this.metroTabPage1.Text = "Контроллер Тип 1";
-            this.metroTabPage1.VerticalScrollbarBarColor = true;
-            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
-            this.metroTabPage1.VerticalScrollbarSize = 10;
+            this.tabControllerStandart.HorizontalScrollbarBarColor = true;
+            this.tabControllerStandart.HorizontalScrollbarHighlightOnWheel = false;
+            this.tabControllerStandart.HorizontalScrollbarSize = 10;
+            this.tabControllerStandart.Location = new System.Drawing.Point(4, 38);
+            this.tabControllerStandart.Name = "tabControllerStandart";
+            this.tabControllerStandart.Size = new System.Drawing.Size(752, 324);
+            this.tabControllerStandart.TabIndex = 0;
+            this.tabControllerStandart.Text = "Стандартный";
+            this.tabControllerStandart.VerticalScrollbarBarColor = true;
+            this.tabControllerStandart.VerticalScrollbarHighlightOnWheel = false;
+            this.tabControllerStandart.VerticalScrollbarSize = 10;
             // 
-            // metroTabPage2
+            // tabControllerClassic
             // 
-            this.metroTabPage2.HorizontalScrollbarBarColor = true;
-            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
-            this.metroTabPage2.HorizontalScrollbarSize = 10;
-            this.metroTabPage2.Location = new System.Drawing.Point(4, 38);
-            this.metroTabPage2.Name = "metroTabPage2";
-            this.metroTabPage2.Size = new System.Drawing.Size(752, 324);
-            this.metroTabPage2.TabIndex = 1;
-            this.metroTabPage2.Text = "Контроллер Тип 2";
-            this.metroTabPage2.VerticalScrollbarBarColor = true;
-            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
-            this.metroTabPage2.VerticalScrollbarSize = 10;
+            this.tabControllerClassic.HorizontalScrollbarBarColor = true;
+            this.tabControllerClassic.HorizontalScrollbarHighlightOnWheel = false;
+            this.tabControllerClassic.HorizontalScrollbarSize = 10;
+            this.tabControllerClassic.Location = new System.Drawing.Point(4, 38);
+            this.tabControllerClassic.Name = "tabControllerClassic";
+            this.tabControllerClassic.Size = new System.Drawing.Size(752, 324);
+            this.tabControllerClassic.TabIndex = 1;
+            this.tabControllerClassic.Text = "Классический";
+            this.tabControllerClassic.VerticalScrollbarBarColor = true;
+            this.tabControllerClassic.VerticalScrollbarHighlightOnWheel = false;
+            this.tabControllerClassic.VerticalScrollbarSize = 10;
             // 
             // metroLabel2
             // 
@@ -86,20 +90,48 @@
             this.metroLabel2.TabIndex = 11;
             this.metroLabel2.Text = "MES GROUP";
             // 
+            // tabControllerHyper
+            // 
+            this.tabControllerHyper.HorizontalScrollbarBarColor = true;
+            this.tabControllerHyper.HorizontalScrollbarHighlightOnWheel = false;
+            this.tabControllerHyper.HorizontalScrollbarSize = 10;
+            this.tabControllerHyper.Location = new System.Drawing.Point(4, 38);
+            this.tabControllerHyper.Name = "tabControllerHyper";
+            this.tabControllerHyper.Size = new System.Drawing.Size(752, 324);
+            this.tabControllerHyper.TabIndex = 2;
+            this.tabControllerHyper.Text = "Гипер";
+            this.tabControllerHyper.VerticalScrollbarBarColor = true;
+            this.tabControllerHyper.VerticalScrollbarHighlightOnWheel = false;
+            this.tabControllerHyper.VerticalScrollbarSize = 10;
+            // 
+            // tabControllerMini
+            // 
+            this.tabControllerMini.HorizontalScrollbarBarColor = true;
+            this.tabControllerMini.HorizontalScrollbarHighlightOnWheel = false;
+            this.tabControllerMini.HorizontalScrollbarSize = 10;
+            this.tabControllerMini.Location = new System.Drawing.Point(4, 38);
+            this.tabControllerMini.Name = "tabControllerMini";
+            this.tabControllerMini.Size = new System.Drawing.Size(752, 324);
+            this.tabControllerMini.TabIndex = 3;
+            this.tabControllerMini.Text = "Мини";
+            this.tabControllerMini.VerticalScrollbarBarColor = true;
+            this.tabControllerMini.VerticalScrollbarHighlightOnWheel = false;
+            this.tabControllerMini.VerticalScrollbarSize = 10;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 446);
             this.Controls.Add(this.metroLabel2);
-            this.Controls.Add(this.metroTabControl1);
+            this.Controls.Add(this.tabControllers);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Resizable = false;
             this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
-            this.Text = "Прошивальщик оборудования Хоботс Л v2";
-            this.metroTabControl1.ResumeLayout(false);
+            this.Text = "Прошивальщик  Хоботс Л";
+            this.tabControllers.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,10 +139,12 @@
 
         #endregion
 
-        private MetroFramework.Controls.MetroTabControl metroTabControl1;
-        private MetroFramework.Controls.MetroTabPage metroTabPage1;
-        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroTabControl tabControllers;
+        private MetroFramework.Controls.MetroTabPage tabControllerStandart;
+        private MetroFramework.Controls.MetroTabPage tabControllerClassic;
         private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTabPage tabControllerHyper;
+        private MetroFramework.Controls.MetroTabPage tabControllerMini;
     }
 }
 
